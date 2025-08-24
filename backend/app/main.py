b3428ccc4e62c3550d28b132f69c3feb217b6335
gittydia <yeogiyeogi394@gmail.com>
2025-08-24 11:49:59 +0800

    Add CORS origin for deployment on Render

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,6 +29,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://ambag.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
